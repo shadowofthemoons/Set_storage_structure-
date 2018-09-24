563b304d5bc30155d03b10dc35de725eafdd9d96
shadowofthemoons <zinckov.artyom@yandex.ru>
2018-09-24 21:50:20 +0400

    create_a_constructor_and_destructor

--- Set_storage_structure/BitField.cpp
+++ Set_storage_structure/BitField.cpp
@@ -1,12 +1,41 @@
 #include "pch.h"
 #include "BitField.h"
+#include <iostream>
+using namespace std;
+
 
 
 TBitField::TBitField(int len)
 {
+	BitLen = len;
+	MemLen = ((len + 31) >> 5);
+	pMem = new uint[MemLen];
+	if (pMem == NULL)
+	{
+		cout << "error in creating an array";
+		exit(1);
+	}
+	for (int i=0;i<MemLen;i++)
+	{
+		pMem[i] = 0;
+	}
 }
-
-
+TBitField::TBitField(const TBitField &bf)
+{
+	BitLen = bf.BitLen;
+	MemLen = bf.MemLen;
+	pMem = new uint[MemLen];
+	if (pMem == NULL)
+	{
+		cout << "error in creating an array";
+		exit(1);
+	}
+	for (int i = 0; i < MemLen; i++)
+	{
+		pMem[i] = bf.pMem[i];
+	}
+}
 TBitField::~TBitField()
 {
+	delete pMem;
 }
--- Set_storage_structure/BitField.h
+++ Set_storage_structure/BitField.h
@@ -1,17 +1,17 @@
 #pragma once
-typedef unsigned int TELEM;
+typedef unsigned int uint;
 class TBitField
 {
 private:
 	int BitLen; // длина битового поля - макс. к-во битов
-	TELEM *pMem; // память для представления битового поля
+	uint *pMem; // память для представления битового поля
 	int MemLen; // к-во эл-тов Мем для представления бит.поля
 	// методы реализации
 //	int GetMemIndex(const int n) const; // индекс в pМем для бита n
-//	TELEM GetMemMask(const int n) const; // битовая маска для бита n
+//	uint GetMemMask(const int n) const; // битовая маска для бита n
 public:
 	TBitField(int len);
-//	TBitField(const TBitField &bf);
+	TBitField(const TBitField &bf);
 	~TBitField();
 	// доступ к битам
 	//int GetLength(void) const; // получить длину (к-во битов)
@@ -26,4 +26,5 @@ public:
 //	TBitField operator~(void); // отрицание
 	//friend istream &operator>>(istream &istr, TBitField &bf);
 	//friend ostream &operator<<(ostream &ostr, const TBitField &bf);
-};
+};
+
